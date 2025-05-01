953f833ab359079c911f035b90d446e56162032a
heeyeon477 <gimh64627@gmail.com>
2025-05-01 22:17:54 +0900

    JDBC 프로젝트 세팅. article write, list

---
+++ AM_JDBC_25.5/src/main/java/koreaIT/Article.java
@@ -0,0 +1,14 @@
+package koreaIT;
+
+public class Article {
+
+    public Article(int lastId, String title, String body) {
+    }
+
+    public Object getId() {
+    }
+
+    public Object getTitle() {
+    }
+}
+
